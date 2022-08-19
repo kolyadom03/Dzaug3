73c70efaf5d6f7073e706859c94de21eb88a53e2
kolya <kolyadomashnikov2003@gmail.com>
2022-08-19 21:59:32 +0300

    first comit

---
+++ nom2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+string number = Console.ReadLine();
+int sum = 0;
+for (int i = 0; i < number.Length; i++)
+{
+    string temp = Convert.ToString(number[i]);
+    sum += Convert.ToInt32(temp);
+}
+Console.WriteLine(sum);
+
+
