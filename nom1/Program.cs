289b602c24aa1b8cb2df896671e342c6a7abb316
kolya <kolyadomashnikov2003@gmail.com>
2022-08-19 21:56:46 +0300

    first comit

---
+++ nom1/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите 2 числа");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = Convert.ToInt32(Console.ReadLine());
+int res = 1;
+for(int i = 1; i <= numberB; ++i)
+    res = res  * numberA;
+        Console.WriteLine(res);
