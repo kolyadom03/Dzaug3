70e2a868f466d54b093408c324e2e1d8e5801ead
kolya <kolyadomashnikov2003@gmail.com>
2022-08-25 19:47:37 +0300

    2 comit

--- nom2/Program.cs
+++ nom2/Program.cs
@@ -1,11 +1,22 @@
-﻿Console.WriteLine("Введите число");
-string number = Console.ReadLine();
+﻿int size = 3;
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
 int sum = 0;
-for (int i = 0; i < number.Length; i++)
+void FillArrayRandomNumbers(int[] array)
 {
-    string temp = Convert.ToString(number[i]);
-    sum += Convert.ToInt32(temp);
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 999);
+    }
 }
-Console.WriteLine(sum);
+void PrintArray(int[] array)
+{
+    for(int i = 1; i < array.Length; i+=2)
+ {
+    sum += array[i];
+ }    
+    
+         Console.Write(sum);
 
-
+}
