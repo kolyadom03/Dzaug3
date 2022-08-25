7b75ba56dd23a7f0e10af890d4567dc60d5166cd
kolya <kolyadomashnikov2003@gmail.com>
2022-08-25 19:47:50 +0300

    3 comit

--- nom3/Program.cs
+++ nom3/Program.cs
@@ -1,5 +1,28 @@
-﻿Console.Write("Введите число");
-int nom = Convert.ToInt32(Console.ReadLine());
-int[] array = {nom};
-for(int i = 0; i < array.Length; i++)
-    Console.Write(array[i]);
+﻿int size = 10;
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+void FillArrayRandomNumbers(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 999);
+    }
+}
+void PrintArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]+ " ");
+    }
+}
+int Max = numbers[0];
+int Min = numbers[0];
+for(int i = 1; i < size; i++)
+{
+    if(Max < numbers[i])
+        Max = numbers[i];
+    if(Min > numbers[i])
+        Min = numbers[i];
+}
+Console.Write(Max + Min); 
