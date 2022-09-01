bb8e73c28c19b795d52e52f9b5fe86b34cadbf8b
kolya <kolyadomashnikov2003@gmail.com>
2022-09-01 11:30:22 +0300

    2 comit

--- nom2/Program.cs
+++ nom2/Program.cs
@@ -1,16 +1,30 @@
-﻿Console.Write("Введите k1: ");
-int k1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите b1: ");
-int b1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите k2: ");
-int k2 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите b2: ");
-int b2 = Convert.ToInt32(Console.ReadLine());
- 
- 
-int x = -(b1 - b2) / (k1 - k2);
-int y = k1 * x + b1;
- 
-
- 
-Console.WriteLine($"Пересечение в точке: ({x};{y})");
+﻿
+        {
+            Console.Write("Введите строку:");
+            int pos1 = Convert.ToInt32(Console.ReadLine()) - 1;
+            Console.Write("Введите столбец:");
+            int pos2 = Convert.ToInt32(Console.ReadLine()) - 1;
+            int n = 5;
+            int m = 7;
+            Random random = new Random();
+            int[,] arr = new int[n, m];
+            Console.WriteLine("Исходный массив:");
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    arr[i, j] = random.Next(10, 99);
+                    Console.Write("{0} ", arr[i, j]);
+                }
+                Console.WriteLine();
+            }
+            if (pos1 < 0 | pos1 > arr.GetLength(0) - 1 | pos2 < 0 | pos2 > arr.GetLength(1) - 1)
+            {
+                Console.WriteLine("Элемент не существует");
+            }
+            else
+            {
+                Console.WriteLine("Значение элемента массива = {0}", arr[pos1, pos2]);
+            }
+            Console.ReadLine();
+        }
