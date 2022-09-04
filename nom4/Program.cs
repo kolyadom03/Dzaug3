ecd6bcc9e4ad3febdd04bdd2ef708ce5f76fe79c
kolya <kolyadomashnikov2003@gmail.com>
2022-09-04 19:53:00 +0300

    4 commit

---
+++ nom4/Program.cs
@@ -0,0 +1,32 @@
+﻿ 
+for (int i = 0; i < arr.GetLength(0); i++)
+ 
+   for (int j = 0; j < arr.GetLength(1); j++)
+    
+        for (int k = 0; k < arr.GetLength(2); k++)
+ 
+            arr[i, j, k] = random.Next(10, 99);
+            int t = arr[i, j, k];
+               int w = 0;
+                if (w ==t)
+                    break;
+                if (w != t)
+                t = w;
+      
+ 
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    Console.WriteLine("Page N: "+ (i + 1));
+ 
+   for (int j = 0; j < arr.GetLength(1); j++)
+       {
+        for (int k = 0; k < arr.GetLength(2); k++)
+            {
+            Console.Write(arr[i, j, k] + " ");
+            }
+        Console.WriteLine();
+       }
+    Console.WriteLine("============================");
+}
+ 
+Console.ReadLine();
