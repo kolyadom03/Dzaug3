ec9ea4812facec9e52233a6675a70253c930de39
kolya <kolyadomashnikov2003@gmail.com>
2022-09-10 15:17:19 +0300

    1 commit

--- nom1/Program.cs
+++ nom1/Program.cs
@@ -1,62 +1,8 @@
-﻿
-Console.WriteLine("Введите число");
-int m = InputNumbers("Введите m: ");
-int n = InputNumbers("Введите n: ");
-int range = InputNumbers("Введите: от 1 до ");
-
-int[,] array = new int[m, n];
-CreateArray(array);
-WriteArray(array);
-
-
-OneArrayLines(array);
-WriteArray(array);
-
-void OneArrayLines(int[,] array)
-{
-  for (int i = 0; i < array.GetLength(0); i++)
-  {
-    for (int j = 0; j < array.GetLength(1); j++)
-    {
-      for (int k = 0; k < array.GetLength(1) - 1; k++)
-      {
-        if (array[i, k] < array[i, k + 1])
-        {
-          int temp = array[i, k + 1];
-          array[i, k + 1] = array[i, k];
-          array[i, k] = temp;
-        }
-      }
-    }
-  }
-}
-
-int InputNumbers(string input)
-{
-  Console.Write(input);
-  int output = Convert.ToInt32(Console.ReadLine());
-  return output;
-}
-
-void CreateArray(int[,] array)
-{
-  for (int i = 0; i < array.GetLength(0); i++)
-  {
-    for (int j = 0; j < array.GetLength(1); j++)
-    {
-      array[i, j] = new Random().Next(range);
-    }
-  }
-}
-
-void WriteArray(int[,] array)
-{
-  for (int i = 0; i < array.GetLength(0); i++)
-  {
-    for (int j = 0; j < array.GetLength(1); j++)
-    {
-      Console.Write(array[i, j] + " ");
-    }
-    Console.WriteLine();
-  }
-}
+﻿Console.Write("Введи N: ");
+int N = Convert.ToInt16(Console.ReadLine());
+if (0>N)
+for (int i = N; i <= 0; i++)
+    Console.Write(i);
+else
+    for (int i = 0; i <= N; i++)
+        Console.Write(i);
