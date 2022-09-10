988c89fc7d5896a037cb250d3be9a8b7f8cac232
kolya <kolyadomashnikov2003@gmail.com>
2022-09-10 15:18:07 +0300

    3 commit

--- nom3/Program.cs
+++ nom3/Program.cs
@@ -1,41 +1,22 @@
-﻿  int[,]x1 = new int[3, 3];
-            int[,]x2 = new int[3, 3];
-            int[,]x3 = new int[3, 3];
- 
-           x1[0,0] = 1;
-           x1[0,1] = 1;
-           x1[0,2] = 2;
- 
-           x1[1,0] = 5;
-           x1[1,1] = 4;
-           x1[1,2] = 3;
- 
-           x1[2,0] = 2;
-           x1[2,1] = 4;
-           x1[2,2] = 9;
- 
- 
-           x2[0,0] = 1;
-           x2[0,1] = 1;
-           x2[0,2] = 5;
- 
-           x2[1,0] = 3;
-           x2[1,1] = 6;
-           x2[1,2] = 8;
- 
-           x2[2,0] = 5;
-           x2[2,1] = 4;
-           x2[2,2] = 6;
-            int j;
-            for (int i = 0; i < 3; i++)
-            {
-                for (j = 0; j < 3; j++)
-                {
-                   x1[i, j] =x1[i, j] *x2[i, j];
-                    Console.Write(x1[i, j]);
-                    Console.WriteLine("\n");
-                }
-                j = 0;
-            }
-            Console.ReadKey();
-        
+﻿int n = InputInt("Введите положительное число");
+int m = 1;
+if (n < 1)
+{
+ Console.WriteLine("Ввели не положительное число");
+}
+Console.WriteLine(NaturalNumber(n, m));
+
+int NaturalNumber(int n, int m)
+{
+ if (n == m)
+ return n;
+ else
+ Console.Write($"{NaturalNumber(n, m + 1)}, ");
+ return m;
+}
+
+int InputInt(string output)
+{
+ Console.Write(output);
+ return int.Parse(Console.ReadLine());
+}
